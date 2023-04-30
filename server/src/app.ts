70aff81ff17df924181444cde7128a00ee98016c
nahee-park <jessica396@ewhain.net>
2023-04-30 21:08:25 +0200

    feat: change domain

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,8 +19,8 @@ export const nftContractInterface = [
   'function balanceOf(address _owner) view returns (uint256)',
 ];
 
-// export const REDIRECT_URL = 'https://www.endolphin.link/';
-export const REDIRECT_URL = 'https://main.dxl01j7nzkmcx.amplifyapp.com';
+export const REDIRECT_URL = 'https://www.endolphin.link/';
+// export const REDIRECT_URL = 'https://main.dxl01j7nzkmcx.amplifyapp.com';
 // export const REDIRECT_URL = 'http://127.0.0.1:5173/';
 export const rpcURL = 'https://polygon-rpc.com';
 export const port = process.env.PORT;
